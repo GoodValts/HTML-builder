c60cd4695594f89468ba8be2e4f7d2856c061582
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-05-05 16:23:32 +0300

    feat: add ability to copy folder  5 май 2023 г. 16:23:32

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -21,7 +21,11 @@ fs.rm(pathToNewFolder, { recursive:true, force:true }, err => {
               let pathToDestFile = pathToNewFolder + '\\' + files[i].name;
               fs.copyFile(pathToSrcFile, pathToDestFile, (err) => {
                 if (err) {
-                  throw err;
+                  if (err.errno === -4048){
+                    copyFolders(err, pathToSrcFile);                  
+                  } else {
+                    throw err;
+                  }
                 }
               });
             }
@@ -31,3 +35,36 @@ fs.rm(pathToNewFolder, { recursive:true, force:true }, err => {
     });
   }
 });
+
+function copyFolders(err, directoryPath) {
+  if (err.errno === -4048) {
+    fs.readdir(directoryPath, {withFileTypes: true}, (err, files) => {
+      if (err) {
+        throw err;
+      } else {
+        let pathToFolder = directoryPath.replace('files', 'files-copy');
+        fs.mkdir(pathToFolder, { recursive: true }, err => {
+          if (err) {
+            throw err;
+          } else {
+            for (let i = 0; i < files.length; i += 1) {
+              let pathToSrcFile = directoryPath + '\\' + files[i].name;
+              let pathToDestFile = pathToSrcFile.replace('files', 'files-copy');
+              fs.copyFile(pathToSrcFile, pathToDestFile, (err) => {
+                if (err) {
+                  if (err.errno === -4048){
+                    copyFolders(err, pathToSrcFile);
+                  } else {
+                    throw err;
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+  } else {
+    throw err;
+  }
+}
