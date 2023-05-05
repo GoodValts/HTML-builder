61a40a164537be13066a7d5bbad350ab94109477
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-05-05 15:33:53 +0300

    feat: add task4 solution  5 май 2023 г. 15:33:53

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,108 @@
+console.log('test4');
 
+const fs = require('fs');
+const path = require('node:path');
+
+let pathToSourseFolder = path.join(__dirname, '\\files');
+let pathToNewFolder = path.join(__dirname, '\\files-copy');
+
+//console.log(pathToNewFolder);
+/*
+
+fs.readdir(__dirname, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    //console.log('files=', files);
+    for (let i = 0; i < files.length; i += 1) {
+      //console.log('---start cycle---');
+      //console.log('files[i].name=', files[i].name);
+      if (files[i].isDirectory() === true && files[i].name === 'files-copy') {
+        //console.log('!!!find files-copy');
+        fs.readdir(pathToNewFolder, {withFileTypes: true}, (err, files) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log(files);
+            for (let i = 0; i < files.length; i += 1) {
+              console.log('zz=', files[i].name);
+              let pathToFile = pathToNewFolder + '\\' + files[i].name;
+              console.log(pathToFile);
+              fs.rm(pathToFile, err => {
+                if (err) {
+                  throw err;
+                } else {
+                  console.log('delete', files[i].name);
+                }
+              });
+            }
+          }
+        });
+      }
+      //console.log('---end cycle---');
+    }
+  }
+});
+
+
+fs.mkdir(path.join(__dirname, '\\files-copy'), { recursive: true }, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('new repo');
+  }
+});
+
+
+fs.readdir(pathToSourseFolder, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    //console.log('files=', files);
+    for (let i = 0; i < files.length; i += 1) {
+      let pathToSrcFile = pathToSourseFolder + '\\' + files[i].name;
+      let pathToDestFile = pathToNewFolder + '\\' + files[i].name;
+      //console.log(pathToSrcFile, '\n', pathToDestFile);
+      fs.copyFile(pathToSrcFile, pathToDestFile, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log ('copy', files[i].name);
+        }
+      });
+    }
+  }
+});*/
+
+fs.rm(pathToNewFolder, { recursive:true, force:true }, err => {
+  if (err) {
+    throw err;
+  } else {
+    fs.mkdir(path.join(__dirname, '\\files-copy'), { recursive: true }, err => {
+      if (err) {
+        throw err;
+      } else {
+        console.log('new repo');
+        fs.readdir(pathToSourseFolder, {withFileTypes: true}, (err, files) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log('files=', files);
+            for (let i = 0; i < files.length; i += 1) {
+              let pathToSrcFile = pathToSourseFolder + '\\' + files[i].name;
+              let pathToDestFile = pathToNewFolder + '\\' + files[i].name;
+              //console.log(pathToSrcFile, '\n', pathToDestFile);
+              fs.copyFile(pathToSrcFile, pathToDestFile, (err) => {
+                if (err) {
+                  throw err;
+                } else {
+                  console.log ('copy', files[i].name);
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+  }
+});
